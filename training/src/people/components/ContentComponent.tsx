47db1ef911f6e6ea5b44aa565676f5b0cdd5136f
JohnGitHubMan <t.huppmann@gmx.de>
2020-08-18 00:23:46 +0200

    nix

--- training/src/people/components/ContentComponent.tsx
+++ training/src/people/components/ContentComponent.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 export default function ContentComponent() { 
     return  (
         <div id="content">
-            <p>Empty Content</p>
+
+<br />
+<br />
+<br />
+<br />
+<p>Empty Content</p>
+<br />
+<br />
+<br />
+<br />
+<br />
+
+
         </div>
       )
 
